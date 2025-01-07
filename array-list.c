ce88e9977caef054570e67126de6464fbed7e6be
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-01-07 13:22:57 -0300

    Change return type of functions
    Change return type from void to ArrayList* to handle errors properly

--- array-list.c
+++ array-list.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void arraylist_append(ArrayList *array_list, int new_element) {}
-void arraylist_insert(ArrayList *array_list, int new_element, unsigned position) {}
-void arraylist_update(ArrayList *array_list, int new_element, unsigned position) {}
-void arraylist_delete(ArrayList *array_list, int new_element, unsigned position) {}
-void arraylist_print(ArrayList *array_list) {}
+ArrayList* arraylist_append(ArrayList *array_list, int new_element) {}
+ArrayList* arraylist_insert(ArrayList *array_list, int new_element, unsigned position) {}
+ArrayList* arraylist_update(ArrayList *array_list, int new_element, unsigned position) {}
+ArrayList* arraylist_delete(ArrayList *array_list, int new_element, unsigned position) {}
+ArrayList* arraylist_print(ArrayList *array_list) {}
